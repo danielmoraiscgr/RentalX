455076863f4753547eb41ecce161cac6fdfa8737
Daniel Morais Borges <daniel.morais@bumlai.com.br>
2022-01-04 15:22:01 -0400

    Upload file and Swagger Doc

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -5,8 +5,6 @@ const specificationsRoutes = Router();
 
 specificationsRoutes.post('/',(request, response ) =>{
     return createSpecificationController.handle(request, response ); 
-    
-
 })
 
 export { specificationsRoutes }
