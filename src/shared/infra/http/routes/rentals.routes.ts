f374536e17ad9021228546630eb171513712156b
Daniel Morais Borges <daniel.morais@bumlai.com.br>
2022-04-15 12:30:49 -0400

    Listing Rentals by user

--- src/shared/infra/http/routes/rentals.routes.ts
+++ src/shared/infra/http/routes/rentals.routes.ts
@@ -2,12 +2,15 @@ import { Router } from "express";
 
 import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticate";
-import { DevolutionRentalController } from "@modules/rentals/useCases/createRental/devolutionRental/DevolutionrentalController";
+import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/DevolutionRentalController";
+import { ListRentalsByUserController } from "@modules/rentals/useCases/listRentalsByUser/ListRentalsByUserController";
+
 
 const rentalsRoutes = Router(); 
 
 const createRentalController = new CreateRentalController(); 
 const devolutionRentalController = new DevolutionRentalController();
+const listRentalsByUserUseCase = new ListRentalsByUserController(); 
 
 rentalsRoutes.post(
     "/",
@@ -17,5 +20,9 @@ rentalsRoutes.post(
 rentalsRoutes.post("/devolution/:id",
     ensureAuthenticated, 
     devolutionRentalController.handle)
+
+rentalsRoutes.get("/user",
+    ensureAuthenticated, 
+    listRentalsByUserUseCase.handle)
 
 export { rentalsRoutes }
